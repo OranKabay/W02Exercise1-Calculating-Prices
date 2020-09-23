9b43eed5107f6188a1ddd1bd8b4bcd75375429d4
Oran <oran@emetrix.biz>
2020-09-23 18:53:27 +0100

    first commit

---
+++ src/W02Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class W02Exercise1 {
+    public static void main(String[] args) {
+        Scanner customer = new Scanner(System.in);
+        System.out.println("Price 1:");
+        double priceOfBurger1 = customer.nextDouble();
+        System.out.println("Price 2:");
+        double priceOfBurger2 = customer.nextDouble();
+        System.out.println("Price 3:");
+        double priceOfBurger3 = customer.nextDouble();
+        double averagePriceOfBurgers = (priceOfBurger1 + priceOfBurger2 + priceOfBurger3)/3;
+        System.out.println("The average age of the three burgers is £" + averagePriceOfBurgers + ".");
+    }
+
+}
